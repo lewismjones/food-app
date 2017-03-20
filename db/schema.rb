c9b3fedbf19eba4e7a1d01e668fd0e08638c0b0d
Lewis Jones <lewismjones@gmail.com>
2017-03-19 20:28:49 -0400

    fix this ish

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170318190952) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
     t.string   "description"
     t.datetime "created_at",  null: false
