00eb010a9399df40d2f0379a0ad7e3ebe67a0c4d
Lewis Jones <lewismjones@gmail.com>
2017-03-19 21:04:26 -0400

    yes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,5 @@
 class Recipe < ApplicationRecord
 belongs_to :category
 has_one :instruction, dependent: :destroy
-has_one :ingredient, , dependent: :destroy
+has_one :ingredient, dependent: :destroy
 end
