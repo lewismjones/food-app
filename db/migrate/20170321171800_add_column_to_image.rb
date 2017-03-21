501a1c81fa9694e6948fadbbdf36a1460ea13424
Lewis Jones <lewismjones@gmail.com>
2017-03-21 14:35:12 -0400

    yee

--- db/migrate/20170321171800_add_column_to_image.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnToImage < ActiveRecord::Migration[5.0]
-  def change
-    add_column :images, :image_url, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170321171800) do
+ActiveRecord::Schema.define(version: 20170321165639) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
